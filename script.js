3d89f7e59798bf3655100b055c3b05031d33908e
Dan Saa <dansaa8@protonmail.com>
2022-10-23 03:30:36 +0200

    Almost finish assignment (final polishing left)

--- script.js
+++ script.js
@@ -1,33 +1,72 @@
-const addBtn = document.querySelector(".container__button");
+const btnAddTask = document.querySelector(".container__button");
 const inputField = document.querySelector(".container__input");
-const ul = document.querySelector(".container__list");
+const ul = document.querySelector(".container__ul");
 const infoToUser = document.querySelector(".container__infoToUser");
+const completedTasks = document.querySelector(".container__completedTasks");
 
 const taskArr = [];
-addBtn.addEventListener("click", function () {
+let count = 0;
 
+btnAddTask.addEventListener("click", function () {
   const userVal = inputField.value;
 
   if (isVarEmpty(userVal) != true) {
+    infoToUser.innerText = "";
+    infoToUser.setAttribute("class", "");
 
     taskArr.push({
       task: userVal,
     });
-    
-    infoToUser.innerText = "";
 
     const li = document.createElement("li");
     ul.appendChild(li);
 
-    const span = document.createElement("span");
-    span.innerText = userVal;
-    li.appendChild(span);
+    const taskInfo = document.createElement("span");
+    taskInfo.innerText = userVal;
+    li.appendChild(taskInfo);
 
     inputField.value = ""; //empty field after a successful item is added
 
+    taskInfo.addEventListener("click", function () {
+      //every span element gets this function
+      if (li.getAttribute("class") == "completed") {
+        li.setAttribute("class", "");
+        count--;
+      } else {
+        li.setAttribute("class", "completed");
+        count++;
+      }
+      completedTasks.innerText = `${count} completed`;
+    });
 
-  } 
+    const trashCan = document.createElement("span");
+    trashCan.innerHTML = "  &#x1F5D1";
+    trashCan.setAttribute("class", "trashcan"); //vad fan gör denna?
+    li.appendChild(trashCan);
+
+    trashCan.addEventListener("click", function () {
+      if (li.getAttribute("class") == "completed") {
+        count--;
+      }
+      removeObjFromArr(taskInfo.innerText);
+      li.remove();
+      completedTasks.innerText = `${count} completed`;
+    });
+  } else {
+    //outer statement
+    infoToUser.innerText = "Input must not be empty";
+    infoToUser.setAttribute("class", "blinkingRed");
+    console.log("empty input");
+  }
 });
+
+function removeObjFromArr(taskToBeRemoved) {
+  for (let i = 0; i < taskArr.length; i++) {
+    if (taskArr[i].task == taskToBeRemoved) {
+      taskArr.splice([i], 1);
+    }
+  }
+}
 
 function isVarEmpty(variable) {
   if (variable.length == 0) {
